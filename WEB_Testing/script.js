dab904dc674758787da2a69e860c6258c0e81441
rishav-jha-mech <rishav.rj74@gmail.com>
2021-10-22 16:16:34 +0530

    Extended Support For Facebook Videos

--- WEB_Testing/script.js
+++ WEB_Testing/script.js
@@ -1,7 +1,6 @@
 function  PostTheData(){
-  url = document.getElementById('ababa').value;
+  url = document.getElementById('theUrl').value;
   axios.post('http://127.0.0.1:8888', {
-    // headers: {"Accept": "application/json"},
     uri: url,
   })
   .then((response) => handleRes(response.data))
@@ -12,8 +11,7 @@ function  PostTheData(){
 
 // For Testing Data
   // axios({
-  //   // url : './response.json',
-  //   url: './response.json',
+  //   url : 'response.json',
   //   method : 'GET',
   // })
   // .then((request) => handleRes(request.data))
@@ -22,10 +20,18 @@ function  PostTheData(){
 var root = document.getElementById('root')
 
 const handleRes = (data) =>{
-  
+
     console.log(data)
 
+    const YOUTUBE = Boolean(data.extractor == "youtube")
+    const FACEBOOK = Boolean(data.extractor == "facebook")
+
+    if(YOUTUBE){ // This was for Youtube (It has 4 thumbnails)
     document.getElementById("thumb").setAttribute("src",data.thumbnails[1].url) // Enough For Mobiles size => (168x94)
+    }
+    else if(FACEBOOK){ // This was for Facebook (It has 1 thumbnail)
+    document.getElementById("thumb").setAttribute("src",data.thumbnails[0].url) // Enough For Mobiles size => (168x94)
+    }
     document.getElementById("title").innerHTML=data.title
     document.getElementById("duration").innerHTML=data.duration + " s"
 
@@ -40,7 +46,7 @@ const handleRes = (data) =>{
       li.setAttribute("class","list-group-item d-flex justify-content-between align-items-center")
       li.innerHTML = `
       
-      <a class="my-1 px-2 py-2" href="${data.requested_formats[i].url}">${data.requested_formats[i].format_note}  ${data.requested_formats[i].format} </a>
+      <a class="my-1 px-2 py-2" href="${data.requested_formats[i].url}" target="_blank">${data.requested_formats[i].format_note}  ${data.requested_formats[i].format} </a>
       <span class="badge bg-primary rounded-pill">${data.requested_formats[i].ext}</span>
       `
       req.appendChild(li);
@@ -51,7 +57,7 @@ const handleRes = (data) =>{
           var li = document.createElement("li");
           li.setAttribute("class","list-group-item d-flex justify-content-between align-items-center")
           li.innerHTML = `
-          <a class="my-1 px-2 py-2" href="${data.formats[i].url}">${data.formats[i].format_note}  <!--${data.formats[i].format}--> </a>
+          <a class="my-1 px-2 py-2" href="${data.formats[i].url}"  target="_blank">${data.formats[i].format_note}  <!--${data.formats[i].format}--> </a>
           <span class="badge bg-primary rounded-pill">${data.formats[i].format_note}</span>
           <span class="badge bg-success rounded-pill">${data.formats[i].ext}</span>
           `
@@ -63,8 +69,15 @@ const handleRes = (data) =>{
       if (!data.formats[i].asr) {
           var li = document.createElement("li");
           li.setAttribute("class","list-group-item d-flex justify-content-between align-items-center")
+
+          strFormat = data.formats[i].format
+          x = strFormat.search("-")
+          realFormat = strFormat.slice(x+2,strFormat.length)
+
           li.innerHTML = `
-          <a class="my-1 px-2 py-2" href="${data.formats[i].url}">${data.formats[i].format_note}  <!--${data.formats[i].format}--> </a>
+          <a class="my-1 px-2 py-2" href="${data.formats[i].url}"  target="_blank">
+            ${realFormat}
+          </a>
           <span class="badge bg-primary rounded-pill">${data.formats[i].format_note}</span>
           <span class="badge bg-success rounded-pill">${data.formats[i].ext}</span>
           `
@@ -77,12 +90,12 @@ const handleRes = (data) =>{
           var li = document.createElement("li");
           li.setAttribute("class","list-group-item d-flex justify-content-between align-items-center")
           li.innerHTML = `
-          <a class="my-1 px-2 py-2" href="${data.formats[i].url}">${data.formats[i].format_note}  <!--${data.formats[i].format}--> </a>
+          <a class="my-1 px-2 py-2" href="${data.formats[i].url}" target="_blank">${data.formats[i].format_note}  <!--${data.formats[i].format}--> </a>
           <span class="badge bg-primary rounded-pill">${data.formats[i].format_note}</span>
           <span class="badge bg-success rounded-pill">${data.formats[i].ext}</span>
           `
           audio_only.appendChild(li);
       }
     }
+
 }
-// }
