ae8fb6ea9f892ef579e8c314c8e67de0682c21a8
rishav-jha-mech <rishav.rj74@gmail.com>
2021-10-22 15:02:48 +0530

    Removed Support for templates

--- app.py
+++ app.py
@@ -3,22 +3,15 @@ import youtube_dl
 import json
 from flask_cors import CORS,cross_origin
 
-app = Flask(__name__,template_folder="templates")
+app = Flask(__name__)
 CORS(app)
 cors = CORS(app, resources={r"/*": {"origins": "127.0.0.1:5500"}})
 
-@app.route('/post',methods=['GET'])
-@cross_origin()
-def send():
-
-	# response.headers.add("Access-Control-Allow-Origin", "*")
-	response.headers.add("Access-Control-Allow-Origin", "*")
-	return render_template("index.html")
 
 @app.route('/', methods=["POST", "GET"])
 @cross_origin()
 def API():
-	
+
 	url = request.get_json().get('uri')
 	with youtube_dl.YoutubeDL() as ydl:
 
